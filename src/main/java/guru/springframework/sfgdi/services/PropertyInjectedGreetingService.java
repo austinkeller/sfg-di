949aa00fa52968de03f0304388208ed8d93588ee
Austin Keller <austin.keller@smartsheet.com>
2021-04-05 23:56:30 -0700

    feat: Add all up through add Profiles

---
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyInjectedGreetingService implements guru.springframework.sfgdi.services.GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - Property";
+    }
+}
